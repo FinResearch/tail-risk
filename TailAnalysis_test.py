93f6bd7ab8413467b61bfc5da22efe43ebf52732
Jethro Cao <jethrocao@gmail.com>
2020-01-20 00:02:24 +0700

    Revert initial_date & fix small type error

--- TailAnalysis_test.py
+++ TailAnalysis_test.py
@@ -102,14 +102,16 @@ def get_tail_stats(fit_obj, tail_data, ks_pvgof_tup):
     return alpha, xmin, s_err, tail_size, ks_pv
 
 
+# NOTE: do the right/left values have to alternate in output CSV?
 def tail_stat_zipper(tstat1, tstat2):
     return [val for pair in zip(tstat1, tstat2) for val in pair]
 
 
+# TODO: merge this extraction function into get_tail_stats()
 def get_logl_tstats(daily_log_ratio, daily_log_pv):
     r_tpl, r_exp, r_logn = daily_log_ratio
     p_tpl, p_exp, p_logn = daily_log_pv
-    return list(r_tpl, r_exp, r_logn, p_tpl, p_exp, p_logn)
+    return list((r_tpl, r_exp, r_logn, p_tpl, p_exp, p_logn))
 
 
 #####################################
@@ -128,8 +130,9 @@ labels = fieldValues
 
 database = Extractor(database_name, labels)
 
-#  initial_date = "1/1/2016"
-initial_date = "2/5/2016"
+# FIXME?: using date below -> ValueError: cannot convert float NaN to integer
+#  initial_date = "2/5/2016"
+initial_date = "1/1/2016"
 final_date = "5/5/2016"
 lookback = 504
 
