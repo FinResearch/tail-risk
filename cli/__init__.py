3f7d9a4ac029361bac0f96ec2fd400a585f27c2c
Jethro Cao <jethrocao@gmail.com>
2020-03-07 14:02:21 +0800

    Restructure: cli/
    * move attach_script_opts decorator into callbacks.py
    * callbacks.py -> dct_cbs.py
    * ui.py -> __init__.py

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,71 +1,8 @@
 import click
-import yaml
 
-# NOTE: imports detected as unused by linter are reified by eval() calls
-from vnargs import VnargsOption
-from callbacks import (gset_db_df,
-                       gset_group_opts,
-                       gset_xmin_args,
-                       confirm_group_flag_set,)
+from dct_cbs import attach_script_opts, gset_db_df, gset_group_opts
 
 #  from statistics import NormalDist
-
-
-def _preprocess_special_attrs_(opt_attrs):
-    """Helper for correctly translating the data types from the YAML config
-    and Python; and to conveniently set some meta attributes.
-
-    This function mutates the passed opt_attrs dict
-    """
-
-    # attrs that are special expression objects
-    expr_attrs = ('type', 'callback', 'cls',)
-    for attr in expr_attrs:
-        # check the attr is specified in the config & its value is truthy
-        if attr in opt_attrs and bool(opt_attrs[attr]):
-            attr_val = opt_attrs[attr]
-            if isinstance(attr_val, str):
-                opt_attrs[attr] = eval(attr_val)
-            elif attr == 'type' and isinstance(attr_val, list):
-                # branch specific to type attrs with list vals
-                opt_attrs['type'] = click.Choice(attr_val)
-            else:  # TODO: revise error message
-                raise TypeError(f'{attr_val} of {type(attr_val)} cannot be '
-                                f'used as the value for the {attr} attribute '
-                                'of click.Option objects')
-
-    # meta attrs that can be optionally passed, to customize info from --help
-    meta_help_attrs = {'show_default': True, 'metavar': None}
-    for attr, dflt in meta_help_attrs.items():
-        opt_attrs[attr] = opt_attrs.get(attr, dflt)
-
-
-# load (from YAML), get & set (preprocess) options attributes
-def _load_gset_opts_attrs():
-
-    cfg_path = 'config/options/attributes.yaml'
-    with open(cfg_path) as cfg:
-        opts_attrs = yaml.load(cfg, Loader=yaml.SafeLoader)
-
-    for opt, attrs in opts_attrs.items():
-        _preprocess_special_attrs_(attrs)
-
-    return opts_attrs
-
-
-# decorator wrapping click.Option's decorator API
-def attach_script_opts():
-    """Attach all options specified within attributes.yaml config file
-    to the decorated click.Command instance.
-    """
-    opts_attrs = _load_gset_opts_attrs()
-
-    def decorator(cmd):
-        for opt in reversed(opts_attrs.values()):
-            param_decls = opt.pop('param_decls')
-            cmd = click.option(*param_decls, **opt)(cmd)
-        return cmd
-    return decorator
 
 
 #  def _get_db_choices():
@@ -91,6 +28,7 @@ def attach_script_opts():
 @click.argument('db_df', metavar='DB_FILE', nargs=1, is_eager=True,
                 type=click.File(mode='r'), callback=gset_db_df,
                 default='dbMSTR_test.csv')  # TODO: allow 2nd pos-arg for cfg?
+# TODO: consider moving DB_FILE & -G into config/options/attributes.yaml ??
 @click.option('-G', '--group/--no-group', 'analyze_group',
               is_eager=True, callback=gset_group_opts,
               default=False, show_default=True,
--- cli/dct_cbs.py
+++ cli/dct_cbs.py
@@ -1,6 +1,71 @@
+import click
 import yaml
 import pandas as pd
 
+# NOTE: import detected as unused by linter, but is reified by eval() call
+from _vnargs import VnargsOption
+
+
+# # Decorator
+
+def __preprocess_special_attrs_(opt_attrs):
+    """Helper for correctly translating the data types from the YAML config
+    and Python; and to conveniently set some meta attributes.
+
+    This function mutates the passed opt_attrs dict
+    """
+
+    # attrs that are special expression objects
+    expr_attrs = ('type', 'callback', 'cls',)
+    for attr in expr_attrs:
+        # check the attr is specified in the config & its value is truthy
+        if attr in opt_attrs and bool(opt_attrs[attr]):
+            attr_val = opt_attrs[attr]
+            if isinstance(attr_val, str):
+                opt_attrs[attr] = eval(attr_val)
+            elif attr == 'type' and isinstance(attr_val, list):
+                # branch specific to type attrs with list vals
+                opt_attrs['type'] = click.Choice(attr_val)
+            else:  # TODO: revise error message
+                raise TypeError(f'{attr_val} of {type(attr_val)} cannot be '
+                                f'used as the value for the {attr} attribute '
+                                'of click.Option objects')
+
+    # meta attrs that can be optionally passed, to customize info from --help
+    meta_help_attrs = {'show_default': True, 'metavar': None}
+    for attr, dflt in meta_help_attrs.items():
+        opt_attrs[attr] = opt_attrs.get(attr, dflt)
+
+
+# load (from YAML), get & set (preprocess) options attributes
+def _load_gset_opts_attrs():
+
+    cfg_path = 'config/options/attributes.yaml'
+    with open(cfg_path) as cfg:
+        opts_attrs = yaml.load(cfg, Loader=yaml.SafeLoader)
+
+    for opt, attrs in opts_attrs.items():
+        __preprocess_special_attrs_(attrs)
+
+    return opts_attrs
+
+
+# decorator wrapping click.Option's decorator API
+def attach_script_opts():
+    """Attach all options specified within attributes.yaml config file
+    to the decorated click.Command instance.
+    """
+    opts_attrs = _load_gset_opts_attrs()
+
+    def decorator(cmd):
+        for opt in reversed(opts_attrs.values()):
+            param_decls = opt.pop('param_decls')
+            cmd = click.option(*param_decls, **opt)(cmd)
+        return cmd
+    return decorator
+
+
+# # Callbacks
 
 # TODO: optimize using list.index(value)?
 def _get_param_from_ctx(ctx, param_name):
