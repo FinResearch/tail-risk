d721fc7306fd7054ee70c5cf7c46353a9b911e51
Jethro Cao <jethrocao@gmail.com>
2020-06-04 21:27:59 -0400

    Check for __main__ for run_analysis_gui.py

--- run_analysis_gui.py
+++ run_analysis_gui.py
@@ -4,9 +4,11 @@ from cli.gui import GUI
 from utils.analysis import analyze_tail
 from utils.settings import Settings as Settings
 
-input_args = GUI().get_cli_input_args()
-# FIXME: multiple GUI boxes popup when using nproc >1
-user_inputs = cli.get_user_inputs.main(args=input_args, standalone_mode=False)
+if __name__ == "__main__":
+    input_args = GUI().get_cli_input_args()
+    # FIXME: multiple GUI boxes popup when using nproc >1
+    user_inputs = cli.get_user_inputs.main(args=input_args,
+                                           standalone_mode=False)
 
-settings = Settings(user_inputs).settings
-analyze_tail(settings)
+    settings = Settings(user_inputs).settings
+    analyze_tail(settings)
