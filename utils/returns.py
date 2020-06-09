3265f5010c0c47977819fde305f9a3fd1de43994
Jethro Cao <jethrocao@gmail.com>
2020-06-08 21:41:43 -0400

    Move normalization related settings into own sub-section
    * new sub-settings section: rtrn (returns)
    * includes normalization related settings

--- utils/returns.py
+++ utils/returns.py
@@ -7,6 +7,7 @@ class Returns:
 
     def __init__(self, settings):
         self.sd = settings.data
+        self.sr = settings.rtrn
         self.sa = settings.anal
 
         returns_df = self.__compute_returns_df()
@@ -15,20 +16,20 @@ class Returns:
         self.normalizer = Normalizer(settings, returns_df)
 
     def __compute_returns_df(self):
-        tau = self.sa.tau
+        tau = self.sr.tau
 
         nan_pad = np.full(tau, np.nan)
-        print(f"{self.sa.returns_type.upper()} returns selected")
+        print(f"{self.sr.returns_type.upper()} returns selected")
         # TODO: move above printing into verbosity logging
 
         def get_returns(series):  # calculates returns for a series
             pt_i = series[:-tau]
             pt_f = series[tau:]
-            if self.sa.returns_type == "raw":
+            if self.sr.returns_type == "raw":
                 returns = pt_f - pt_i
-            elif self.sa.returns_type == "relative":
+            elif self.sr.returns_type == "relative":
                 returns = pt_f / pt_i - 1.0
-            elif self.sa.returns_type == "log":
+            elif self.sr.returns_type == "log":
                 returns = np.log(pt_f/pt_i)
             return np.hstack((nan_pad, returns))
 
@@ -62,6 +63,7 @@ class _Normalizer(ABC):
     def __init__(self, settings, returns_df):
         # TODO: take input vector directly from data settings?
         self.sd = settings.data
+        self.sr = settings.rtrn
         self.sa = settings.anal
         self.returns_df = returns_df
 
@@ -70,9 +72,9 @@ class _Normalizer(ABC):
         pass
 
     def __normalize_numpy(self, X):  # X must be a numpy.ndarray
-        if self.sa.standardize:
+        if self.sr.standardize:
             X = (X - X.mean()) / X.std(ddof=1)
-        if self.sa.absolutize:
+        if self.sr.absolutize:
             X = np.abs(X)
         return X
 
@@ -84,7 +86,7 @@ class _Normalizer(ABC):
         data = data[~np.isnan(data)]
 
         # normalize after grouping in -G mode
-        if self.sa.norm_after:
+        if self.sr.norm_after:
             # TODO/ASK: take care of when to absolutize for -G, since --norm_after is the default
             # ASK/TODO: do --std & --abs necessarily have the same timing when both are specified???
             data = self.__normalize_numpy(data)
@@ -97,7 +99,7 @@ class StaticNormalizer(_Normalizer):
         super().__init__(settings, returns_df)
         assert not self.sa.use_dynamic
 
-        if self.sa.standardize:  # TODO: calc always if getting moments here
+        if self.sr.standardize:  # TODO: calc always if getting moments here
             # means & stds should be Pandas Series here (1 moment per ticker)
             self.means = self.returns_df.mean()  # NOTE: NaNs ignored by dflt
             self.stds = (self.returns_df.std()   # NOTE: NaNs ignored by dflt
@@ -108,9 +110,9 @@ class StaticNormalizer(_Normalizer):
     def _norm_tickers(self, norm_id):
         group = norm_id
         data = self.returns_df[group]
-        if self.sa.standardize:
+        if self.sr.standardize:
             data = self.stdzd_cols_df[group]
-        if self.sa.absolutize:
+        if self.sr.absolutize:
             data = np.abs(data)
         return data
 
@@ -128,9 +130,9 @@ class DynamicNormalizer(_Normalizer):
         self._win_type = {'rolling': 'rolling', 'increasing': 'expanding'}.\
             get(self.sa.approach)
         self.data_window = getattr(self.returns_df,
-                                   self._win_type)(self.sa.dyn_win_size)
+                                   self._win_type)(self.sr.dyn_win_size)
 
-        if self.sa.standardize:  # TODO: calc always if getting moments here
+        if self.sr.standardize:  # TODO: calc always if getting moments here
             # means & stds should be Pandas DataFrame here (1 moment per date)
             self.means = self.__get_window_stat('mean')
             self.stds = (self.__get_window_stat('std')
@@ -141,12 +143,12 @@ class DynamicNormalizer(_Normalizer):
         assert len(window_stat) == len(self.sd.anal_dates),\
             (f'cannot calculate suitable {stat.upper()}s for analysis dates '
              f"[{', '.join(self.sd.anal_dates)}] from {self._win_type} "
-             f'window of minimum size {self.sa.dyn_win_size}, constructed '
+             f'window of minimum size {self.sr.dyn_win_size}, constructed '
              f'from DataFrame below:\n\n{self.returns_df}\n')
         return window_stat
 
     def __get_lookback_label(self, date):
-        lkb = (self.dates.get_loc(date) - self.sa.dyn_win_size + 1
+        lkb = (self.dates.get_loc(date) - self.sr.dyn_win_size + 1
                if self.sa.approach == 'rolling' else 0)
         assert lkb >= 0  # this necessarily must be True, otherwise bug
         return self.dates[lkb]
@@ -157,10 +159,10 @@ class DynamicNormalizer(_Normalizer):
 
         data = self.returns_df.loc[lkbd:date, group]
 
-        if self.sa.standardize:
+        if self.sr.standardize:
             stat_loc = (date, group)
             data = (data - self.means.loc[stat_loc]) / self.stds.loc[stat_loc]
-        if self.sa.absolutize:
+        if self.sr.absolutize:
             data = np.abs(data)
 
         return data
